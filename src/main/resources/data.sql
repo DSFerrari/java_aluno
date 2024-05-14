44a987accc177e85f2ead904d481b8dc757aaff0
DSFerrari <diegoferrari02022022@gmail.com>
2024-05-14 11:06:03 -0300

    banco de dados funcional

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into TBL_STUDENT(NAME, COURSE) VALUES ('Diego','Computação')
+insert into TBL_STUDENT(NAME, COURSE) VALUES ('Thiago','História')
+insert into TBL_STUDENT(NAME, COURSE) VALUES ('Italo','Matemática')
